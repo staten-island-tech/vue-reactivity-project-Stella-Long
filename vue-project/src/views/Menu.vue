f8e78bd60c066e9fbc3435815d773cf2d14dd62b
Stella-Long <huierl@nycstudents.net>
2026-02-11 12:31:37 -0500

    s

---
+++ vue-project/src/views/Menu.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Menu</h1>
+        <h2>{{ count }}</h2>
+        <button @click="increment">add to cart</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(0)
+function increment() {
+    count.value++
+}
+</script>
+
+<style scoped>
+.button {
+    width: 200px;
+}
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+</style>
