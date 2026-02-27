0a911db5a1b59722425f2e6bc04dd084cfdd3b11
Stella-Long <huierl@nycstudents.net>
2026-02-27 12:32:04 -0500

    add cart

--- vue-project/src/views/Cart.vue
+++ vue-project/src/views/Cart.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>Menu</h1>
     </div>
-    <div v-for="drink in drinks" :key="drink.id">
+    <div v-for="drink in drinks" :key="drink.name">
       <p>{{ drink.name }} - ${{ drink.price }}</p>
       <button @click="addToCart(drink)">add to cart</button>
   </div>
@@ -13,12 +13,16 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+function addToCart(drink) {
+  console.log(drink)
+}
+const drinks = ref([
+  {name: 'Jasmine Latte', price: '$6.49'},
+  {name: 'Coconut Mango Boom', price: '$7.49'},
+  {name: 'Triple Matcha Supreme', price: '$7.99' },
+  {name: 'GrapefruitBoom', price: '$7.49'},
+  {name: 'Crispy Grape', price: '$7.49'},
 
-function addToCart()
-const drinks = ref([s
-  { id:1, name: 'Jasmine Latte', price: '$6.49', img: ''},
-  { id:2, name: 'Coconut Mango Boom', price: '$7.49', img: ''},
-  { id:3, name: 'Triple Matcha Supreme', price: '$7.99', img: '' },
 ])
 </script>
 
