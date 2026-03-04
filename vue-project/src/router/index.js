caf9e893f94dabe3b9d9a7fcd0fae310f162486f
Stella-Long <huierl@nycstudents.net>
2026-03-04 12:28:27 -0500

    s

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Menu from '@/views/Cart.vue'
+import Cart from '@/views/Cart.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- vue-project/src/views/Cart.vue
+++ vue-project/src/views/Cart.vue
@@ -8,7 +8,7 @@
   </div>
   <h2>Cart</h2>
   <p v-if="cart.length === 0">empty</p>
-  
+  <p v-else="cart.length === ">{{ drink.name }}</p>  
 </template>
 
 <script setup lang="ts">
@@ -18,14 +18,13 @@ function addToCart(drink) {
   cart.value.push(drink)
 }
 const drinks = ref([
-  {name: 'Jasmine Latte', price: '$6.49'},
-  {name: 'Coconut Mango Boom', price: '$7.49'},
-  {name: 'Triple Matcha Supreme', price: '$7.99' },
-  {name: 'GrapefruitBoom', price: '$7.49'},
-  {name: 'Crispy Grape', price: '$7.49'},
+  {name: 'Jasmine Latte', price: '6.49'},
+  {name: 'Coconut Mango Boom', price: '7.49'},
+  {name: 'Triple Matcha Supreme', price: '7.99' },
+  {name: 'Grapefruit Boom', price: '7.49'},
+  {name: 'Crispy Grape', price: '7.49'},
 ])
 </script>
 
 <style scoped>
-
 </style>
