8713e88f724ab1dccfeaf989f6455f3adf61c231
Stella-Long <huierl@nycstudents.net>
2026-03-24 12:28:53 -0400

    s

--- vue-project/src/views/Cart.vue
+++ vue-project/src/views/Cart.vue
@@ -1,28 +1,4 @@
 <template>
-  <div class="shop">
-    <div class="title">
-      HEYTEA Menu
-    </div>
-    <div class="menu">
-      <div 
-        class="drink"
-        v-for="drink in drinks"
-        :key="drink.name"
-        >
-        <img
-          class="img"
-          :src="drink.img"
-          :alt="drink.name"
-        />
-        <div class="name">
-          {{ drink.name }}
-        </div>
-        <div class="price">
-
-        </div>
-      </div>
-    </div>
-  </div>
     <div>
         <h1>Menu</h1>
     </div>
@@ -33,10 +9,7 @@
   </div>
   <h2>Cart</h2>
   <p v-if="cart.length === 0">empty</p> 
-  <p v-else="drink in drinks" :key="drink.name">
-    <p>{{ drink.name }} - ${{ drink.price }}</p>
-      <button @click="addToCart(drink)">add to cart</button>
-  </p>
+  <p v-else ></p>
 </template>
 
 <script setup lang="ts">
@@ -46,11 +19,11 @@ function addToCart(drink) {
   cart.value.push(drink)
 }
 const drinks = ref([
-  {id: 1, name: 'Jasmine Latte', price: '6.49', img: },
-  {id: 2, name: 'Coconut Mango Boom', price: '7.49', img: },
-  {id: 3, name: 'Triple Matcha Supreme', price: '7.99', img: },
-  {id: 4, name: 'Grapefruit Boom', price: '7.49', img:},
-  {id: 5, name: 'Crispy Grape', price: '7.49', img: },
+  {id: 1, name: 'Jasmine Latte', price: '6.49'},
+  {id: 2, name: 'Coconut Mango Boom', price: '7.49'},
+  {id: 3, name: 'Triple Matcha Supreme', price: '7.99' },
+  {id: 4, name: 'Grapefruit Boom', price: '7.49'},
+  {id: 5, name: 'Crispy Grape', price: '7.49'},
 ])
 </script>
 
